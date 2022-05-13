a7df595a849d212f3faa2758bc7c6fcaa3d58b71
Humberto Alejandro Rosas Téllez <93540685+Humbert100@users.noreply.github.com>
2022-05-12 22:52:13 -0500

    Añadí nombre de autor

--- memory.py
+++ memory.py
@@ -1,12 +1,5 @@
 """Memory, puzzle game of number pairs.
-
-Exercises:
-
-1. Count and print how many taps occur.
-2. Decrease the number of tiles to a 4x4 grid.
-3. Detect when all tiles are revealed.
-4. Center single-digit tile.
-5. Use letters instead of tiles.
+Author: Humberto Alejandro Rosas Téllez
 """
 
 from random import *
