87d1b6d84e388532cfdfcc484c1548f6739392f8
Humberto Alejandro Rosas Téllez <93540685+Humbert100@users.noreply.github.com>
2022-05-12 22:58:58 -0500

    Añadí maneras de juego

--- memory.py
+++ memory.py
@@ -48,6 +48,23 @@ def tap(x, y):
         state['pairs'] += 1 #Contador de pares
         state['mark'] = None
 
+  '''
+                                    Juego Completo
+    Si usted desea encontrar todos los pares del memorama, debe de descomentar el 
+    siguiente bloque de código y comentar el código que comienza con el if state['pairs'] ==1: 
+    '''
+    # if not any(hide): #Comprueba si ningún cuadro está oculto, muestra el total de taps realizados
+    #     write_total() #Llama función write_total(); imprime "You win!" y "Total:"  
+
+    ##                       Juego terminado al encontrar 1 pares de números                 
+    ## Se comprueba si el número de pares es igual a 1; puede modificarlo para un mayor entretenimiento
+    
+    if state['pairs'] == 1: 
+        win() #Llamar a función win();imprime imagen
+        write_total() #Llamar a función write_total(); muestra "You win!" y "Total:"                
+    else: #Si no se cumple condición anterior, se continúa con el conteo de taps
+        state['taps'] += 1 
+        write_taps() #Llamar a función write_taps(); enseña "Taps"         
 
 def draw():
     """Draw image and tiles."""
